1a792b88049f1ef6a44a77ca0cb4edc1cb3668c4
Héctor <hsoriano9986@gmail.com>
2024-06-26 12:20:20 +0200

    migraciones actualizadas debido a error en su creación, db operativa

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,5 +1,9 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { CreateExperiences1719394829424 } from "../entities/experiences/1719394829424-createExperiences";
+import { Experience } from "../entities/experiences/experienceModel";
+import { CreateUsers1719389972894 } from "../entities/users/1719389972894-createUsers";
+import { User } from "../entities/users/userModel";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -10,11 +14,13 @@ export const AppDataSource = new DataSource({
     database: "RegisterExercise",
     // entities: [`${__dirname}/../models/**/*{.js,.ts}`],
     entities: [
-        
+        User,
+        Experience
     ],
     // migrations: [`${__dirname}/migrations/**/*{.js,.ts}`],
     migrations: [
-       
+        CreateUsers1719389972894,
+        CreateExperiences1719394829424
     ],
     synchronize: false,
     logging: false,
